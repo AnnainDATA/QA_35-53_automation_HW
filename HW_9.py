11c314087d50fc9b0ad7fca11c50e147bf29fd1c
Anna <anna.anikeenko85@gmail.com>
2026-08-12 23:10:33 +0300

    HW9

---
+++ HW_9.py
@@ -0,0 +1,59 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def pause(seconds):
+    time.sleep(seconds)
+
+driver=webdriver.Chrome()
+def selectors_phonebook_login():
+    try:
+        driver.get("https://telranedu.web.app/login")
+        pause(2)
+        print("--------------------")
+
+        head2=driver.find_element(By.TAG_NAME,"head")
+        head1=driver.find_element(By.CSS_SELECTOR, "head")
+        print(head1.tag_name)
+        print(head2.tag_name)
+        print("--------------------")
+
+        h1=driver.find_element(By.TAG_NAME,"h1")
+        h1_1=driver.find_element(By.CSS_SELECTOR,"h1")
+        print(h1.tag_name)
+        print(h1_1.tag_name)
+        print("--------------------")
+
+        script=driver.find_element(By.TAG_NAME,"script")
+        script1 = driver.find_element(By.CSS_SELECTOR, "script")
+        print(script.tag_name)
+        print(script1.tag_name)
+        print("--------------------")
+
+        clas_s=driver.find_element(By.CLASS_NAME,"active")
+        clas_s1 = driver.find_element(By.CSS_SELECTOR, ".active")
+        print(clas_s.tag_name)
+        print(clas_s1.tag_name)
+        print("--------------------")
+
+        color=driver.find_element(By.CLASS_NAME,"navbar-component_nav__1X_4m")
+        color1 = driver.find_element(By.CSS_SELECTOR, ".navbar-component_nav__1X_4m")
+        print(color.tag_name)
+        print(color1.tag_name)
+        print("--------------------")
+
+        container=driver.find_element(By.CLASS_NAME,"container")
+        container1 = driver.find_element(By.CSS_SELECTOR, ".container")
+        print(container.tag_name)
+        print(container1.tag_name)
+        print("--------------------")
+
+        root=driver.find_element(By.ID,"root")
+        root1 = driver.find_element(By.CSS_SELECTOR, "#root")
+        print(root.tag_name)
+        print(root1.tag_name)
+        print("--------------------")
+
+    finally:
+        driver.quit()
+selectors_phonebook_login()
